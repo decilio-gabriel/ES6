ea8915191e73d9b360be174fc36e4aba3b24e6d0
Gabriel Decilio <gabrieldecilio@iMac-de-Gabriel.local>
2019-10-11 17:00:59 -0300

    ES6 aprendizagem

---
+++ bundle.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var arr = [1, 3, 4, 5, 6, 7];
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
---
+++ main.js
@@ -0,0 +1,5 @@
+const arr = [1, 3, 4, 5, 6, 7];
+
+const newArr = arr.map(function (item) {
+  return item * 2;
+});
